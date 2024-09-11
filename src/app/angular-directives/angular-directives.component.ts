94882466b8cffa8c5927f34f1b64b727c1626f0c
vishal magdum personal <vishalmagdum17899@gmail.com>
2024-09-11 16:52:16 +0530

    angular change detection

--- src/app/angular-directives/angular-directives.component.ts
+++ src/app/angular-directives/angular-directives.component.ts
@@ -1,10 +1,11 @@
-import { NgIf } from '@angular/common';
+import { NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'angular-directives',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, NgTemplateOutlet, NgFor],
   templateUrl: './angular-directives.component.html',
   styleUrl: './angular-directives.component.css',
 })
@@ -13,8 +14,27 @@ export class AngularDirectivesComponent {
   userName: string = 'John Doe';
   countLogin: number = 0;
   userRole: string = 'admin';
+  usersObj: Array<any> = [
+    { id: 1, name: 'John Doe', role: 'admin' },
+    { id: 2, name: 'Jane Doe', role: 'user' },
+    { id: 3, name: 'John Smith', role: 'admin' },
+    { id: 4, name: 'Jane Smith', role: 'user' },
+  ];
+  users: string[] = ['John Doe', 'Jane Doe', 'John Smith', 'Jane Smith'];
   handleLogin() {
     this.countLogin++;
     console.log(this.countLogin);
+  }
+  addUser() {
+    this.usersObj.push({
+      id: this.usersObj.length + 1,
+      name: 'Raj Patil',
+      role: 'admin',
+    });
+  }
+  deleteUser(user: object) {
+    // this.usersObj = this.usersObj.filter((user) => user.id !== userId);
+    let index = this.usersObj.indexOf(user);
+    this.usersObj.splice(index, 1);
   }
 }
