ea1b0cdf9095fdf781a09e5e9bb3023c9dd68ee0
vishal magdum personal <vishalmagdum17899@gmail.com>
2024-10-07 12:53:30 +0530

    ngComponentOutlet

--- src/app/angular-lifecycle/angular-lifecycle.component.ts
+++ src/app/angular-lifecycle/angular-lifecycle.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Renderer2,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
+import { PostListComponent } from '../post-list/post-list.component';
+import { NgComponentOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-angular-lifecycle',
   standalone: true,
-  imports: [],
+  imports: [PostListComponent, NgComponentOutlet],
   templateUrl: './angular-lifecycle.component.html',
-  styleUrl: './angular-lifecycle.component.css'
+  styleUrl: './angular-lifecycle.component.css',
 })
 export class AngularLifecycleComponent {
+  // constructor(private viewContainer: ViewContainerRef) {}
+  constructor(private render: Renderer2) {}
+  @ViewChild('viewContainer', { read: ViewContainerRef })
+  viewContainer!: ViewContainerRef;
 
+  loadComponent() {
+    // return PostListComponent;
+    // this.viewContainer.createComponent(PostListComponent);
+    const text = this.render.createText(
+      'to show text/component programmatically we are using ngComponentOutlet'
+    );
+    this.viewContainer.element.nativeElement.appendChild(text);
+  }
 }
